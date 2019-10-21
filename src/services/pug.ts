3ddfa7ea379570ca6b2e42115eb2ca07419644db
David Melo <davidmelo@desmdesigns.com>
2019-10-21 16:33:15 -0400

    Updates Path To Template File

--- src/services/pug.ts
+++ src/services/pug.ts
@@ -2,7 +2,7 @@ import pug from 'pug';
 
 const RenderTemplate = function(template: string, options: object): Promise<string> {
     return new Promise<string>((resolve, reject) => {
-        let html = pug.renderFile(`./templates/${template}.pug`, options);
+        let html = pug.renderFile(`./${template}.pug`, options);
         return resolve(html);
     });
 };
